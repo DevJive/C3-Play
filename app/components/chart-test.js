354d2c4ed80b5c6c5d71792ddb660c0215821f2a
BillAtFundy <Bill Campbell>
2015-04-23 16:28:15 -0700

    add test chart component

---
+++ app/components/chart-test.js
@@ -0,0 +1,20 @@
+import Ember from "ember";
+
+export default Ember.Component.extend({
+	
+	title : "Test Chart",
+
+	chart : function(){
+		c3.generate(
+			{
+	    		bindto: '#chart',
+	    		data: {
+	      			columns: [
+	        			['data1', 30, 200, 100, 400, 150, 250],
+	        			['data2', 50, 20, 10, 40, 15, 25]
+	      			]
+	    		}
+			});
+	}
+
+});
